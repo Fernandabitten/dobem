549fe1b7c5e1ed1b4800493471e99c3dd574e092
Fernandabitten <fernandab.bittencourt@gmail.com>
2021-12-21 12:13:11 -0300

    update

--- static/scripts/to-give.js
+++ static/scripts/to-give.js
@@ -31,49 +31,65 @@ $(document).ready(() => {
           let telephone = Number(`${data[key].telephone}`);
 
           if (data[key].type === "noel" && data[key].statusOrder === true) {
-            textReferenceNoel += `<li class="card_text" style="border-style: outset; margin:30px; padding: 20px; text-align: left">             
+            textReferenceNoel += `<li id=${data[key].id} class="card_text" style="border-style: outset; margin:30px; padding: 20px; text-align: left">             
             Doe para: ${data[key].username}<br />         
             Pedido: ${data[key].title}<br /> 
             Descrição: ${data[key].description}<br /><br/> 
-            <p><button class="btn send-button"><a href="https://wa.me/55${telephone}"style="color: #f9da82;">DOAR</a></button></p> 
+            <button style="color: #f9da82;" data-request-id=${data[key].id} class="btn send-button" >DOAR</button>
             </li>`;
             $("#you-noel").html(textReferenceNoel);
           } else if (
             data[key].type === "edu" &&
             data[key].statusOrder === true
           ) {
-            textReferenceEdu += `<li class="card_text" style="border-style: outset; margin:30px; padding: 20px; text-align: left">
+            textReferenceEdu += `<li id=${data[key].id} class="card_text" style="border-style: outset; margin:30px; padding: 20px; text-align: left">
             Doe para: ${data[key].username}<br />    
             Pedido: ${data[key].title}<br />
             Descrição: ${data[key].description}<br /><br/> 
-            <p ><button class="btn send-button" ><a href="https://wa.me/55${telephone}" style="color: #f9da82;">DOAR</a></button></p> 
+            <button style="color: #f9da82;" data-request-id=${data[key].id} class="btn send-button" >DOAR</button>
             </li>`;
             $("#you-edu").html(textReferenceEdu);
           } else if (
             data[key].type === "caregiver" &&
             data[key].statusOrder === true
           ) {
-            textReferenceCaregiver += `<li class="card_text" style="border-style: outset; margin:30px; padding: 20px; text-align: left">
+            textReferenceCaregiver += `<li id=${data[key].id} class="card_text" style="border-style: outset; margin:30px; padding: 20px; text-align: left">
             Doe para: ${data[key].username}<br />    
             Pedido: ${data[key].title}<br />
             Descrição: ${data[key].description}<br /><br/> 
-            <p><button class="btn send-button">           
-            <a href="https://wa.me/55${telephone}"style="color: #f9da82;">
-            DOAR</a></button></p> 
+            <button style="color: #f9da82;" data-request-id=${data[key].id} class="btn send-button" >DOAR</button>
             </li>`;
             $("#you-caregiver").html(textReferenceCaregiver);
-          }
-        }
+          };
+        };
+
+        $(".send-button").on("click", function() {
+          const requestId = $(this).attr("data-request-id");
+          $.ajax({
+            url: "/request-done",
+            type: "PATCH",
+            data: {"data": requestId},
+            success: (data) => {
+              $("#"+requestId).html(`
+                <span>Pedido aceito. Combine a entrega dos produtos pelo WhatsApp.</span><br />
+                <span>Pedido: ${data.title}</span><br />
+                <span>Fale com: ${data.username}</span><br />
+                <button style="color: #f9da82;" class="btn send-button" ><a style="color: #f9da82;" href="https://wa.me/55${data.phone}" target="_blank">Abrir WhatsApp</a></button>
+              `);
+            },
+            error: (data) => {
+              $("#"+requestId).html(`
+                <span>Ocorreu um erro ao tentar verificar<br />as informações deste pedido.<br />Tente atualizar a página.</span><br />
+              `);
+            }
+          });
+        });        
+
       },
     });
   };
   loadMyRequests();
 
-  $(".send-button").on("click", () => {
-    alert(
-      "Pedido finalizado, Combine a melhor forma de envio com o beneficiario pelo whatsap."
-    );
-  });
 
   $(".modal-toggle").on("click", function (e) {
     e.preventDefault();
